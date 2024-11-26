7c432805c2df2c39d5dc638d898b6e287b64d78c
JonathanCadena28906 <jonathancadenaloaiza@gmail.com>
2024-11-26 10:21:53 -0500

    Clases del modelo

--- cpu/unidad_control.py
+++
@@ -1,3 +0,0 @@
-class unidad_control:
-   def __init__(self):
-      pass
--- memoria/programa.py
+++
@@ -1,7 +0,0 @@
-from memoria.memoria import Memoria
-
-
-class MemoriaPrograma(Memoria):
-    def __init__(self, tamaño):
-        super().__init__(tamaño)
-
